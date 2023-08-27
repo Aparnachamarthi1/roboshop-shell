54eb79b563575a89a4e735c05042cb6552387888
Aparna <chamirthiaparna.com>
2023-08-27 15:03:28 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -78,7 +78,7 @@ systemctl start catalogue &>>$LOGFILE
 
 VALIDATE $? "starting catalogue"
 
-cp /home/centos/robotshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "copying mongo repo"
 
